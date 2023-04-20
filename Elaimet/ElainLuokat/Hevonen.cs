772236879f1841632e83f36de7950b681bfd2521
QJUXUQ <justiina.laitinen@hotmail.com>
2023-04-20 18:15:40 +0300

    Luokkakaavioon muutoksia: Hevonen lisätty. Logiikkatehtävää varten Hevosen luokkaa on muutettu samanlaiseksi kuin muiden eläimien, eli luokkakaavion kuvassa Hevonen on tyhjä

--- Elaimet/ElainLuokat/Hevonen.cs
+++ Elaimet/ElainLuokat/Hevonen.cs
@@ -5,48 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ElainLuokat
-{   //Esitellään luokka eli class. Koodi olisi voinut olla Program.cs tiedostossa mutta selkeyden takia luomme jokaisen luokan
-    //omaan tiedostoonsa
-    public class Hevonen
+{                           //Logiikkatehtävää ja luokkakaaviota varten Hevonen-olio on tyhjennetty samanlaiseksi kuin Kissa ja Koira-oliot ja periytyminen Nisäkkäät-luokkaan
+    public class Hevonen : Nisakkaat
     {
-        private string nimi;
-        private int paino;
-        //Alla konstruktori jossa ei ola parametreja, joten kaikki luokasta 
-        //synnytetyt oliot ovat samanlaisia. Konstruktorin tunnistaa siitä
-        // ettei siinä ole palautustyyppiä ja metodin nimi on sama kuin luokannimi
-        public Hevonen()
-        {
-            //Konstruktorissa voi olla oletusarvoja
-            //Luokan sisällä voidaan käyttää privaatti kenttiä
-            nimi = "";
-            paino = 0;
-        }
-        //getterillä ja setterillä annetaan luokan ulkopuolisille sovelluksille mahdollisuus 
-        //asettaa tai hakea kenttiä
-        public string Nimi
-        {
-            get
-            {
-                return nimi;
-            }
-
-            set
-            {
-                nimi = value;
-            }
-        }
-
-        public int Paino
-        {
-            get
-            {
-                return paino;
-            }
-
-            set
-            {
-                paino = value;
-            }
-        }
-    }
+        
+    }   
 }
---
+++ Elaimet/Logiikkaharjoitus/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Logiikkaharjoitus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many animals would you like to create?: ");
+
+            int createAnimals = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i<createAnimals; i++) 
+            {
+                
+            }
+
+        }
+    }
+}
